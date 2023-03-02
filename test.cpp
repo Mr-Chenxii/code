f492a40183457807bfc6a6a775f38a8df291e9cc
Mr-Chenxii <1019766824@qq.com>
2023-03-02 14:33:17 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,21 @@
+/*
+ * @Author: Chenxi
+ * @Date: 2023-03-02 13:46:04
+ * @LastEditors: Chenxi
+ * @LastEditTime: 2023-03-02 14:17:25
+ * @FilePath: /code/test.cpp
+ * @Description: Code Learning
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int c =10;
+    char a[10] ="hello";
+    char b[]={};
+    //strcpy(b,a)
+    cout<<strcpy(b,a)<<endl;
+    cout<<"hello world"<<endl;
+    return 0;
+}
