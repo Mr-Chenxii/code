74d10f74f98831d11dbc5ff8fe6e22947471df4a
Mr-Chenxii <1019766824@qq.com>
2023-04-07 13:01:52 +0800

    test

--- test2.cpp
+++ test2.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: Chenxi
+ * @Date: 2023-04-07 12:59:19
+ * @LastEditors: Chenxi
+ * @LastEditTime: 2023-04-07 13:01:41
+ * @FilePath: /code/test2.cpp
+ * @Description: Code Learning
+ */
 /*** 
  * @Author: Chenxi
  * @Date: 2023-03-02 16:42:00
@@ -10,5 +18,6 @@
 using namespace std;
 int main(){
     cout<<"hello world"<<endl;
+    cout<<"1"<<endl;
     return 0;
 }
